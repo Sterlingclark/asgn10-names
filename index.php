ceb1e9e6601879253c2089e30f50151fe61ccc29
Sterling Clark <sterlingdclark13@gmail.com>
2021-04-27 22:22:28 -0400

    Set up arrays for first, last, and unique names. adding functions next

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+// include '';
+$fileName = 'names-short-list.txt';
+
+$lineNumber = 0;
+
+// load up the array
+$FH = fopen("$fileName", "r");
+$nextName = fgets($FH);
+
+while(!feof($FH)) {
+  if($lineNumber % 2 == 0) {
+    $fullName[] = trim(substr($nextName, 0, strpos($nextName, " --00")));
+  }
+
+$lineNumber++;
+
+$nextName = fgets($FH);
+}
+
+
+
+
+
+
+// get all first names
+foreach($fullNames as $fullName) {
+  $startHere = strpos($fullName, ",") + 1;
+  $firstNames[] = trim(substr($fullname, $startHere));
+}
+
+// get all last names
+foreach($fullNames as $fullName) {
+  $stopHere = strpos($fullName, ",");
+  $lastNames[] = substr($fullName, 0, $stopHere);
+}
+
+// get valid names 
+for($i = 0; $i < sizeof($fullNames); $i++) {
+  // jam the first and last name together without a comma, then rest for alpha-only characters
+  if(ctype_alpha($lastNames[$i].$firstNames[$i])) {
+    $validFirstNames[$i] = $firstNames[$i];
+    $validLastNames[$i] = $lastNames[$i];
+    $validFullNames[$i] = $validLastNames[$i] . "," . $validFirstNames[$i];
+  }
+}
+
+// Display results 
+
+echo '<h1>Names - Results</h1>';
+echo "<p>There are " . sizeof($fullNames) . " total names</p>";
+echo '<ul style="list-style-type:none">';
+  foreach($fullNames as $fullName) {
+    echo "<li>$fullName</li>";
+  }
+echo "</ul>";
+
+echo '<h2>All Valid Names</h2>';
+echo "<p>There are " . sizeof($validFullNames) . " valid names</p>";
+
+
+
+?>
